9eabf78732a7999584d2c7ca7add12f06777d4a2
Jason M. Aron <jasonaronart@gmail.com>
2018-02-09 00:28:21 -0500

    Crystal-Collector-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,12 @@
-// -------- Game Logic -------- //
+//=============================
+// -------- Game Logic -------- 
+//=============================
+
 // Game Start 
 	//Random number = (19 - 120). Set at the beginning of every game.
 		//Rng to create a number and push it to the display.
 
-	//The four crystals = (1 -12).(Buttons).onClicks
+	//The four crystals = (1 - 12).(Buttons).onClicks
 		//Assign hidden values to each crystal.
 		//On click will add a specific amount to the player's score.
 
@@ -14,67 +17,97 @@
 			//lose++
 
 // Game restarts if player wins or loses.
-// -------- End Game Logic --------//
 
-// ------- Notes (OOP) -------- //
-	// ---- Classes ---- //
-	// Constructor- a special method for creating and initializing an object created within a class.
-	// Classes- are "special functions", and just as you can define function expressions and function declarations, the class syntax has two components: class expressions and class declarations.
-	// Class declaration- To declare a class, you use the class keyword with the name of the class ("Rectangle" here).
-	// ---- State & Behavior ---- //
-	// Lamp is an object.
-	// The "state" in lamp:on and off.
-	// The "behavior" in lamp:turn on and turn off.
-// -------- End Notes -------- //
+// -------- End Game Logic --------
 
 
-// -------- So much pseudo code and notes...now do things (9-_-)9 -------- //
+//================================================
+// -------- 2(-_-2) Now do things (9-_-)9 --------
+//================================================
+
 
 var gameElements = {
 
+	winCount: 0,
+	lossCount: 0,
+	targetNumber: 0,
+	playerNumber: 0,
+	
+
 	// Increase Win
-
+	increaseWinCount() {
+		this.winCount++;
+	}	
 	// Increase Loss
-
+	increaseLossCount() {
+		this.lossCount++;
+	}
 	// NewRandomNumber
-
+	NewTargetNumber() {
+		this.targetNumber = Math.floor((Math.random() * 102) + 19);
+	}
 	// Add points to players score
+	addPlayerPoints() {
+		this.playerNumber += ???;
+	}
 
 	// Did user Win
+	didUserWin() {
+		if (this.playerNumber === this.targetNumber) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	// Did user lose
+	didUserLose() {
+		if (this.playerNumber > this.targetNumber) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 };
 
 
-// -------- Game Controls -------- //
+// -------- Crystal Variables -------- 
+
+
+var crystal1 =
+var crystal2 =
+var crystal3 =
+var crystal4 =
+
+
+// -------- Game Controls -------- 
+
 
 var gameControls = {
-	// On click
-		// Specify what crystal was clicked
-		// Add crystals points to score
-		// Did user win
-			// Restart Round
-			// wins++
-		// Did user lose
-			//Restart round
-			//lose++
-	// Reset game.Elements
-		// crystal1
-		// crystal2
-		// crystal3
-		// crystal4
+
+	$("#crystal1").on("click", function() {
+	}
+	$("#crystal2").on("click", function() {
+	}
+	$("#crystal3").on("click", function() {
+	}
+	$("#crystal4").on("click", function() {
+	}	
+// On click
+// Specify what crystal was clicked
+// Add crystals points to counter
+// Did user win
+	// Restart Round
+	// wins++
+// Did user lose
+	//Restart round
+	//lose++
+// Reset game.Elements	
 };
 
 
-// -------- Crystal Variables -------- //
+// -------- View -------- 
 
-var crystal1 
-var crystal2 
-var crystal3 
-var crystal4 
-
-
-// -------- View -------- //
 
 	// Wins
 	// Loss
